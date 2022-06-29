e0778d309003c0d3fd8c00b3db52d8f906fab2d6
Java Master Jerôme Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2022-06-29 12:44:48 +0200

    Module 8 is done.

---
+++ Paladin/Controllers/EMarketingController.cs
@@ -0,0 +1,45 @@
+﻿using Paladin.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace Paladin.Controllers
+{
+    public class EMarketingController : Controller
+    {
+        private PaladinDbContext _context;
+
+        public EMarketingController(PaladinDbContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult WeeklyReport(EWeeklyReport weeklyReport)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.WeeklyReports.Add(weeklyReport);
+                _context.SaveChanges();
+                return Content("OK");
+            }
+
+            return Content("Error");
+        }
+
+        public ActionResult MonthlyReport(EMonthlyReport monthlyReport)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.MonthlyReports.Add(monthlyReport);
+                _context.SaveChanges();
+                return Content("OK");
+            }
+
+            return Content("Error");
+        }
+    }
+}
